ae1264c7bbfcc55fd0138828740da4e5c4d368f1
Nikol <nikol@gmail.com>
2026-06-26 13:05:55 +0300

    Добавлена валидация форм продуктов

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,15 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
+
+from catalog.forms import ProductForm
+from catalog.models import Product
 
 
 class HomeView(TemplateView):
@@ -7,3 +18,34 @@ class HomeView(TemplateView):
 
 class ContactsView(TemplateView):
     template_name = "catalog/contacts.html"
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "catalog/product_list.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:product_list")
+
