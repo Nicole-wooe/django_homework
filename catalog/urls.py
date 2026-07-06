d05b1360977aa2cbbd4c7b1a3d973f78ac7e398f
Nikol <nikol@gmail.com>
2026-07-06 18:39:37 +0300

    Добавлена аутентификация пользователей

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
 
-from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import (
+    ContactsView,
+    HomeView,
+    ProductCreateView,
+    ProductDeleteView,
+    ProductDetailView,
+    ProductListView,
+    ProductUpdateView,
+)
 
-app_name = CatalogConfig.name
+app_name = "catalog"
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
+    path("", HomeView.as_view(), name="home"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("products/", ProductListView.as_view(), name="product_list"),
+    path("products/create/", ProductCreateView.as_view(), name="product_create"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("products/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
+
+from catalog.forms import ProductForm
+from catalog.models import Product
 
 
-def home(request):
-    return render(request, "catalog/home.html")
+class HomeView(TemplateView):
+    template_name = "catalog/home.html"
 
 
-def contacts(request):
-    return render(request, "catalog/contacts.html")
+class ContactsView(TemplateView):
+    template_name = "catalog/contacts.html"
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "catalog/product_list.html"
+
+
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:product_list")
